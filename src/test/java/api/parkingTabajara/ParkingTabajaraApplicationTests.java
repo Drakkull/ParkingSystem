7ed3cadb8871af7e4814c69bc108015711a79d49
Leonardo DiAbreu <drakkull007@hotmail.com>
2023-09-25 15:34:31 -0300

    First Commit - Parking Tajara

---
+++ src/test/java/api/parkingTabajara/ParkingTabajaraApplicationTests.java
@@ -0,0 +1,13 @@
+package api.parkingTabajara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingTabajaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
